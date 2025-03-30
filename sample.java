04d4fc52c71f73728d6424b433e375ace7c2f201
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-29 19:28:43 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -12,3 +12,44 @@ class Solution {
         return new ArrayList<String>(result);
     }
 }
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Map<String,List<String>> usersong = new HashMap<>();
+        Map<String,List<String>> genresong = new HashMap<>();
+        Map<String,List<String>> usergenreresult = new HashMap<>();
+        Map<String, String> songgenre = new HashMap<>();
+        for (String genre : genresong.keySet() ) {
+            List<String> allsongs = genresong.get(genre);
+            for (String singlesong:allsongs) {
+                songgenre.put(singlesong, genre);
+            }
+        }
+        for (String user: usersong.keySet()) {
+            int max=0;
+            List<String> songlist = usersong.get(user);
+            Map<String, Integer>> genrecount = new HashMap<>();
+            for (String song:songlist) {
+                String genrename = songgenre.get(song);
+                if(!genrecount.containsKey(genrename)) {
+                    genrecount.put(genrename, 1);
+                } else {
+                    genrecount.put(genrename, genrecount.get(genrename)+1);
+                }
+                max = Math.max(max, genrecount.get(genrename));
+            }
+            for (String genrenameid : genrecount.keySet()) {
+                if (max == genrecount.get(genrenameid)) {
+                    if(!usergenreresult.containsKey(user)) {
+                        List<String> genrelist = new ArrayList<>();
+                        genrelist.add(genrenameid);
+                        usergenreresult.put(user, genrelist);
+                    } else {
+                        usergenreresult.get(user).add(genrenameid);
+                    }
+                }
+            }
+            return usergenreresult;
+        }
+
+    }
+}
